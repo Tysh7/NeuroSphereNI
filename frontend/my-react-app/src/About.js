5112935f7725162023839ea3bfa7d6afd1322d37
Tysh7 <tashkunaka@gmail.com>
2024-02-21 23:02:14 +0000

    Added About Page- with who we are info

---
+++ frontend/my-react-app/src/About.js
@@ -0,0 +1,19 @@
+// About.js
+import React from 'react';
+import styles from './About.module.css'; // Import CSS module for the About page styles
+
+const About = () => {
+  return (
+    <div className={styles.aboutContainer}>
+      <h1 className={styles.heading}>Who We Are</h1> {/* Add a class name for the heading */}
+      <div className={styles.underline}></div> {/* Add a div for the underscore */}
+      <p>This is the About page content. It provides information about NeuroSphere NI.</p>
+      <p>orem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed lectus nulla. Duis convallis molestie dolor ac tempor. Sed vel pellentesque massa, id placerat urna. In hac habitasse platea dictumst. Sed tristique enim suscipit, porttitor dolor at, ultricies augue. Aliquam velit leo, pharetra id elit vitae, dapibus egestas neque. Praesent dignissim ut augue non mattis. Vivamus convallis iaculis tristique. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque at pellentesque tortor, vitae pretium augue. Sed pretium vitae est vel viverra. Morbi sit amet egestas nulla. Duis viverra arcu id quam aliquet lacinia. Nullam malesuada placerat lobortis.</p>
+    </div>
+
+
+
+  );
+}
+
+export default About;
--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -1,29 +1,35 @@
+// App.js
 import React from 'react';
-import FlashcardsSection from './FlashCardsSection'; // Import the FlashcardsSection component
+import FlashcardsSection from './FlashCardsSection';
 import styles from './App.module.css';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import About from './About';
 
 function App() {
   return (
-    <div>
-      <header className={styles.header}>
-        <h1>Welcome to NeuroSphere NI</h1>
-        <nav className={styles.navbar}>
-          <ul>
-            <li><a href="#" className={styles.navLink}>ABOUT</a></li>
-            <li><a href="#" className={styles.navLink}>RESOURCES</a></li>
-            <li><a href="#" className={styles.navLink}>ARTICLES</a></li>
-            <li><a href="#" className={styles.navLink}>ACTIVITIES</a></li>
-            <li><a href="#" className={styles.navLink}>CONTACT</a></li>
-          </ul>
-        </nav>
-      </header>
-      <div className={styles.banner}></div>
-      <main>
-        <p>This is the main content of my homepage.</p>
-        <FlashcardsSection /> {/* Include the FlashcardsSection component here */}
-      </main>
-    </div>
+    <Router>
+      <div>
+        <header className={styles.header}>
+          <h1>Welcome to NeuroSphere NI</h1>
+          <nav className={styles.navbar}>
+            <ul>
+              <li><Link to="/about" className={styles.navLink}>ABOUT</Link></li>
+              <li><a href="#" className={styles.navLink}>RESOURCES</a></li>
+              <li><a href="#" className={styles.navLink}>ARTICLES</a></li>
+              <li><a href="#" className={styles.navLink}>ACTIVITIES</a></li>
+              <li><a href="#" className={styles.navLink}>CONTACT</a></li>
+            </ul>
+          </nav>
+        </header>
+        <div className={styles.banner}></div>
+        <main>
+          <Routes>
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
